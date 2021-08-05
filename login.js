6c983c9fbe7a9eec395b29afb4ae3c9a51daa0fd
UsedDiscord <57113628+UsedDiscord@users.noreply.github.com>
2021-08-05 11:43:41 -0700

    login.js
    Changement :
    Remove Token
    Add command
    Refonte total

--- login.js
+++ login.js
@@ -1,40 +1,73 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const snipes = new Discord.Collection();
+const {token} = require('./config.json');
 const activities_list = [
-    "2 mois et 1 jour",
-    "2 mois et 2 jours"
+    "2 mois et 1 jour", //Pourquoi pas pris en compte?
+    "2 mois et 1 jour"
 ];
 
 client.once('ready', () => {
-    console.log('Ready!');
-    setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
-        client.user.setActivity(activities_list[index], {type: 'WATCHING'});
-    }, 15000);
+    console.log('Ready!' + 'La connexion est affecté à : ' + `${client.user.tag}`);
+    client.user.setActivity('En Couple avec Galaxy et Elyvne ; 2 Mois & 6 Jours ❤', "PLAYING");
 });
 
-client.login('SECRETONGITGUB');
+client.login(token);
 
 client.on('message', message => {
-    console.log(message.content);
+    console.log(message.author.tag + ':' + message.content + ' (ID AUTH :)' + message.author.id);
+    //client.channels.cache.get('872863333504667668').send(message.author.tag + ' : ' + message.content + ' : ' + ' ID AUTHOR : ' + message.author.id)
 });
 
 client.on('messageDelete', message => {
     console.log(`LOG CONSOLE: Un message de ${message.author.tag} vient d'être supprimé ! mais nous savons pas qui l'a fait..`);
-    message.channel.send(`Un message de <@${message.author.id}> vient de disparaître ! mais nous savons pas qui l'a fait..`);
+    //message.channel.send('*Attention, bot en developemment, peut provoquer problème. Si problème, merci de signaler a Galaxy. Navré en cas de pb');
+    //message.channel.send(`Un message de <@${message.author.id}> vient de disparaître ! mais nous savons pas qui l'a fait..`);
+    snipes.set(message.channel.id, message)
+
+    const LogChannel = client.channels.cache.get('872863333504667668')
+    const DeletedLog = new Discord.MessageEmbed()
+        .setTitle("Message Supp")
+        .addField('Delete by', message.author)
+        .addField("In", message.channel)
+        .addField('Content', message.content)
+        .setColor('RANDOM')
+        .setThumbnail(message.author.displayAvatarURL({dynamic: true}))
+        LogChannel.send(DeletedLog)
 });
 
 client.on('message', message => {
-    if (message.content === '!ping') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('Pong.');
+    // Ignore messages that aren't from a guild
+    if (!message.guild) return;
+    if (message.content.startsWith('!ping')) {
+        message.channel.send("Pinging...").then(m => {
+            // The math thingy to calculate the user's ping
+            var ping = m.createdTimestamp - message.createdTimestamp;
+
+            // Basic embed
+            var embed = new Discord.MessageEmbed()
+                .setAuthor(`Votre ping est de : ${ping}`)
+                .setColor("RED")
+
+            // Then It Edits the message with the ping variable embed that you created
+            m.edit(embed)
+        });
     }
-    if (message.content === '!admin') {
-        // send back "Le créateur de ce bot est : Galaxy." to the channel the message was sent in
-        message.channel.send('Le Créateur de ce bot est : <@485789557858631680>.');
+    ;
+    if (message.content.startsWith('!mp')) {
+        message.channel.send("ㅤ").then(m => {
+            // The math thingy to calculate the user's ping
+            var ping = m.createdTimestamp - message.createdTimestamp;       //Bon, ne sert à rien mais blc
+
+            // Basic embed
+            var embed = new Discord.MessageEmbed()
+                .setAuthor(`Je viens dans moins de 10 minutes dans votre boîte à lettre privée !`)
+                .setColor("RED")
+
+            // Then It Edits the message with the ping variable embed that you created
+            m.edit(embed)
+            var mp = message.author.send("Your message here.")
+        });
     }
-    if (message.content === '!history') {
-        // send back "Le salon content l'historique des actions est le suivant :" to the channel the message was sent in
-        message.channel.send('Le salon ayant un historique du serveur est le suivant : <#871069630536511538> (ATTENTION : Acces Only LOVEMAX)');
-    }
+    ;
 });
